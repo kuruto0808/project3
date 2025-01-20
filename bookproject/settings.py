db5037711fbf55a0457cdae9c8ec658e7c538f56
kuruto0808 <kuru.tatu@gmail.com>
2025-01-20 11:57:49 +0900

    'deploy'

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['anime-p-262b708fd31e.herokuapp.com/']
+ALLOWED_HOSTS = ['rec-anime-project-9be53b68e139.herokuapp.com']
 
 
 # Application definition
@@ -109,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
