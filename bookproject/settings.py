add10f9db48af66f7fc2a7cb4d6a13c0f969b2a3
kuruto0808 <kuru.tatu@gmail.com>
2025-01-27 02:30:37 +0900

    Update message with relevant description

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -27,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*']
-#ALLOWED_HOSTS = ['rec-anime-site-b1ce77d612af.herokuapp.com']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rec-anime-site-b1ce77d612af.herokuapp.com']
 
 
 # Application definition
